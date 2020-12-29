30d9dfcc6f1b9c04f64166cf5332c5213fa94619
Fatih-Cyber <71564472+Fatih-Cyber@users.noreply.github.com>
2020-12-30 01:39:54 +0300

    Day 7 Sorting added.

---
+++ DataStructures-09-Sorting/src/SortingMainApp.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class SortingMainApp {
+    public static void main(String[] args) {
+        var myarray=new int[]{8, 2, 4, 1, 3};
+        var quickSort= new MyQuickSort();
+        quickSort.sort(myarray);
+        System.out.println(Arrays.toString(myarray));
+    }
+
+}
