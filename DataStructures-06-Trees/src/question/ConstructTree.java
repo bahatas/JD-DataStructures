38710017658698ef8b39b2ff0a62a8cb8b2e29c9
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2022-01-02 23:44:55 +0000

    Tree Review

---
+++ DataStructures-06-Trees/src/question/ConstructTree.java
@@ -0,0 +1,28 @@
+package question;
+
+ class ConstructTree {
+
+}
+
+class Node{
+     int data;
+     Node left,right;
+
+     //Function to create a new binary tree node having a given key
+
+}
+
+/**
+ * Construct the following tree
+ *                    1
+ *                  /   \
+ *                 /     \
+ *                2       3
+ *               /      /   \
+ *              /      /     \
+ *             4      5       6
+ *                   / \
+ *                  /   \
+ *                 7     8
+ *
+ */
