5c41575627fbc822f2dc23e70918d9012153154d
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2022-07-01 00:43:04 +0100

    Tree traverse

--- DataStructures-06-Trees/src/review/Review2.java
+++ DataStructures-06-Trees/src/review/Review2.java
@@ -9,14 +9,14 @@ public class Review2 {
 
     }
 
-    class TTree{
+    class TTree {
 
-        private class NNode{
+        private class NNode {
             private NNode leftChild;
             private NNode rightChild;
             private int value;
 
-            public NNode( int value) {
+            public NNode(int value) {
                 this.value = value;
             }
         }
@@ -25,21 +25,76 @@ public class Review2 {
 
 //        My insert method for Binary Search Tree
 
-        public void insertNode(int value){
+        public void insertNode(int value) {
             NNode node = new NNode(value);
 
             //if tree is empty
-            if(root==null){
+            if (root == null) {
                 root = node;
                 return;
             }
 
-
-
-
-
             //if tree is not empty
+            NNode current = root;
+
+            while (true) {
+//                check if less
+                if (value < current.value) {
+                    if (current.leftChild == null) {
+                        current.leftChild = node;
+                        break;
+
+                    }
+                    current = current.leftChild;
+                } else {
+                    if (current.rightChild == null) {
+                        current.rightChild = node;
+                        break;
+                    }
+                    current = current.rightChild;
+                }
+            }
+        }
+
+        //traverse Pre order Root - Left - Right
+        public void traversePreOrder() {
+
+            traversePreOrder(root);
+        }
+
+        private void traversePreOrder(NNode root) {
+            if (root == null) return;
+            System.out.print(root.value + ",");
+            traversePreOrder(root.leftChild);
+            traversePreOrder(root.rightChild);
 
         }
+
+
+        // traver in order left root right
+        public void traverseInOrder() {
+            traverseInOrder(root);
+        }
+
+        private void traverseInOrder(NNode root) {
+
+            //if root is null
+            if(root == null) return;
+            traversePreOrder(root.leftChild);
+            System.out.println(root.value + ", ");
+            traverseInOrder(root.rightChild);
+
+        }
+
+        // traverse PostOrder  left - right child
+         public void traversePostOrder(){
+            traversePostOrder(root);
+         }
+         private void traversePostOrder(NNode root){
+            if(root == null) return;
+            traversePostOrder(root.leftChild);
+            traversePostOrder(root.rightChild);
+             System.out.println(root.value + ", ");
+         }
     }
 }
